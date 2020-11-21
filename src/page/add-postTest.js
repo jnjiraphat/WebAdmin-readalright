f9e214bbe32b8a1ff310c3f03d8c481851a67528
LAPTOP-30IO2MBF\Jenny <jnjiraphat@gmail.com>
2020-11-21 17:14:46 +0700

    edit format json for post posttest

--- src/page/add-postTest.js
+++ src/page/add-postTest.js
@@ -24,25 +24,90 @@ const AddPostTest = () => {
   console.log("reading id in add post test")
   console.log(match.params.readingId2)
 
+  async function putPostTest(
+    content
+  ) {
+    console.log("content in put post tes")
+    // console.log(dataArrayQuizChallenge)
+    console.log(content)
+    console.log(content[0].questionText)
+    console.log(content[1].questionText)
+    console.log(content[2].questionText)
+    // console.log(dataArrayQuizChallenge[0][0].question_id)
+    for (let index = 0; index < 3; index++) {
+      // const response = await axios.put("http://localhost:3000/admin/updateQuiz/" + dataArrayQuizChallenge[0][index].question_id, {
+      //   question: content[index].questionText,
+      //   typeOfSuggestion_id: 1,
+      //   reading_id: match.params.reading_id,
+      //   typeOfQuestion: "chal"
+      // });
+      // console.log("quiz", response.data);
+    }
+  }
 
-  //ตรงนี้เลยมุกตรงนี้ๆๆๆๆ ขอแค่ URL ที่ get มา เดี๋ยวเราไปต่อเองๆๆๆ
-  // async function editPostTest() {
-  //   console.log("vocab box ID in editVocabBox")
-  //   console.log(match.params.vocabBox_id)
-  //   const result = await axios.get("http://localhost:3000/vocabBox/id/" + match.params.vocabBox_id);
-  //   console.log("result")
-  //   console.log(result.data.reading[0])
-  //   console.log(result.data.reading.length)
-  //   for (let index = 0; index < result.data.reading.length; index++) {
-  //     vocabId.push(result.data.reading[index].vocabCard_id)
-  //     console.log(vocabId[index])
-  //   }
-  //   setTitle(result.data.reading[0].boxEngName)
-  //   setTitleMeaning(result.data.reading[0].boxThaiName)
-  //   setCategoryId(result.data.reading[0].category_id)
-  //   setImage(result.data.reading[0].image)
-  //   setVocabCardID(result.data.reading[0].vocabCard_id)
-  // }
+  async function putPostTestAnswer(
+    content
+  ) {
+    console.log("choice in put post test lenghtttt")
+    console.log(content[0].options.length)
+    console.log("choice in put post test")
+    console.log(content[0].options[0].choice_id)
+
+
+    console.log(content[0].options[0].choice_id)
+    console.log(content[0].options[0].choice)
+    console.log(content[0].options[0].isRightChoice)
+    console.log(content[0].options[0].optionText) 
+    console.log(content[0].options[1].choice_id)
+    console.log(content[0].options[1].choice)
+    console.log(content[0].options[2].choice_id)
+    console.log(content[0].options[2].choice)
+    console.log(content[0].options[3].choice_id)
+    console.log(content[0].options[3].choice)
+    console.log(content[0].options[3].isRightChoice)
+    // console.log(content[0].options[4].choice_id)
+    // console.log(content[0].options[4].choice)
+    console.log(content[1].options[0].choice_id)
+    console.log(content[1].options[0].choice)
+
+    console.log(content[1].options[1].choice_id)
+    console.log(content[1].options[1].choice)
+    console.log(content[1].options[2].choice_id)
+    console.log(content[1].options[2].choice)
+    console.log(content[1].options[3].choice_id)
+    console.log(content[1].options[3].choice)
+
+
+    for (let index = 0; index < content[0].options.length; index++) {
+      const response = await axios.put("http://localhost:3000/admin/updateChoice/" + content[0].options[index].choice_id, {
+        isRightChoice: content[0].options[index].isRightChoice,
+        choice: content[0].options[index].optionText,
+        question_id: content[0].options[index].question_id,
+        optionText: content[0].options[index].optionText,
+        value: content[0].options[index].optionText
+      });
+    }
+
+    for (let index = 0; index < content[1].options.length; index++) {
+      const response = await axios.put("http://localhost:3000/admin/updateChoice/" + content[1].options[index].choice_id, {
+        isRightChoice: content[1].options[index].isRightChoice,
+        choice: content[1].options[index].optionText,
+        question_id: content[1].options[index].question_id,
+        optionText: content[1].options[index].optionText,
+        value: content[1].options[index].optionText
+      });
+    }
+
+    for (let index = 0; index < content[2].options.length; index++) {
+      const response = await axios.put("http://localhost:3000/admin/updateChoice/" + content[2].options[index].choice_id, {
+        isRightChoice: content[2].options[index].isRightChoice,
+        choice: content[2].options[index].optionText,
+        question_id: content[2].options[index].question_id,
+        optionText: content[2].options[index].optionText,
+        value: content[2].options[index].optionText
+      });
+    }
+  }
 
 
   async function postQuiz(
@@ -71,6 +136,25 @@ const AddPostTest = () => {
     console.log("choice", response.data);
   }
 
+  const options = [
+    {
+      optionText: "",
+      isRightChoice: ""
+    },
+    {
+      optionText: "",
+      isRightChoice: ""
+    },
+    {
+      optionText: "",
+      isRightChoice: ""
+    },
+    {
+      optionText: "",
+      isRightChoice: ""
+    },
+  ];
+
   return (
     <Background>
       <Container>
@@ -87,32 +171,17 @@ const AddPostTest = () => {
                     {
                       questionText: "",
                       typeOfSuggestionID: "",
-                      option: [
-                        {
-                          optionText: "",
-                          isRightChoice: ""
-                        }
-                      ]
+                      options: options
                     },
                     {
                       questionText: "",
                       typeOfSuggestionID: "",
-                      option: [
-                        {
-                          optionText: "",
-                          isRightChoice: ""
-                        }
-                      ]
+                      options: options
                     },
                     {
                       questionText: "",
                       typeOfSuggestionID: "",
-                      option: [
-                        {
-                          optionText: "",
-                          isRightChoice: ""
-                        }
-                      ]
+                      options: options
                     },
                   ],
                 }}
@@ -185,14 +254,20 @@ const AddPostTest = () => {
                     }
                     console.log("countQ3");
                     console.log(countQ3);
-                    if (countQ1 == 1 && countQ2 == 1 && countQ3 == 1) {
+                    if (countQ1 == 1 && countQ2 == 1 && countQ3 == 1 && values.content[0].typeOfSuggestionID && values.content[1].typeOfSuggestionID && values.content[2].typeOfSuggestionID) {
                       console.log("Post in this condition");
                       alert(JSON.stringify(values, null, 0));
+                      putPostTest(
+                        values.content
+                      );
+                      putPostTestAnswer(
+                        values.content
+                      );
                     } else {
                       console.log("not success");
                       alert(
                         JSON.stringify(
-                          "Please edit Question1 correct choice just 1"
+                          "Form is not Complete"
                         )
                       );
                     }
@@ -344,14 +419,15 @@ const AddPostTest = () => {
                                         <TextForm>typeOfSuggestion:</TextForm>
                                       </Col>
                                       <Col span="14">
-                                        <FieldStyled
+                                        <Field
                                           as="select"
                                           name={`content.${index}.typeOfSuggestionID`}
                                         >
+                                          <option value="0">Select</option>
                                           <option value="1">Verb</option>
                                           <option value="2">Noun</option>
                                           <option value="3">Adverb</option>
-                                        </FieldStyled>
+                                        </Field>
                                       </Col>
                                     </RowStyled>
                                   </div>
@@ -363,7 +439,7 @@ const AddPostTest = () => {
                     </FieldArray>
 
                     <AreaSubmit>
-                      <ButtonStyled type="submit">Save</ButtonStyled>
+                      <button type="submit">Save</button>
                     <Link to="/">
                       <button>
                         Go to Admin Board
